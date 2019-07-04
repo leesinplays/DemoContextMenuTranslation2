0cfa0ff1786c4dd739ce644651d41789f8237c2b
SOO YONG JIE AARON <18016599@rp.edu.sg>
2019-07-04 09:47:35 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/democontextmenutranslation2/MainActivity.java
@@ -0,0 +1,51 @@
+package sg.edu.rp.c346.democontextmenutranslation2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvTranslatedText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvTranslatedText = findViewById(R.id.textViewTranslatedText);
+        registerForContextMenu(tvTranslatedText);
+
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        // …
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.EnglishSelection) {
+            tvTranslatedText.setText("Hello ");
+            return true;
+        }else if (id == R.id.ItalianSelection) {
+            tvTranslatedText.setText("Ciao");
+            return true;
+        }else  {
+            tvTranslatedText.setText("Error translation");
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
+
+}
